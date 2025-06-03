6db7cff3ce2c0f06d9e04b6796723f80c1fb7849
Ferid Mirzeyev <feridmrzyv2005@gmail.com>
2025-06-03 14:05:47 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,23 @@ import Home from "./pages/Home";
 import Playlists from "./pages/Playlists";
 
 export default function App() {
-
   useEffect(() => {
-    fetch('https://muzimap.svdev.me/spotify-token')
-    .then((response) => response.json())
-    .then(x => localStorage.setItem('spotify-token', x.access_token))
+    fetch("https://muzimap.svdev.me/spotify-token")
+      .then((response) => response.json())
+      .then((x) => localStorage.setItem("spotify-token", x.access_token));
   }, []);
 
   return (
-  
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/aboutus" element={<AboutUs/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/premium" element={<Premium />} />
-          <Route path="/playlists" element={<Playlists/>} />
-        </Routes>
-        <Footer />
-      </div>
-
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/premium" element={<Premium />} />
+        <Route path="/playlists" element={<Playlists />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
